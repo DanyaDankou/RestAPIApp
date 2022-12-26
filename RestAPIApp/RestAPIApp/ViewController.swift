a1130ffc51d8c9e8c40f1e98807bd12f39d981c6
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-12-26 16:34:15 +0300

    Project Created

---
+++ RestAPIApp/RestAPIApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RestAPIApp
+//
+//  Created by comp on 26.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
